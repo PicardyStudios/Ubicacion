8ed900310d310d3e42c028e3c5aa5edfa019f58f
Mr Vince <marvince.ar@gmail.com>
2019-04-11 14:41:23 -0300

    11/04/19 - 14.41

--- app/src/main/java/com/picardystudios/ubicacion/MainActivity.java
+++ app/src/main/java/com/picardystudios/ubicacion/MainActivity.java
@@ -1,53 +1,58 @@
 package com.picardystudios.ubicacion;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
+
+
+
 import android.location.Location;
-import android.location.LocationListener;
+
+import android.os.Bundle;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.RetryPolicy;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+
+import android.content.pm.PackageManager;
+
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.yanzhenjie.permission.AndPermission;
-import com.yanzhenjie.permission.runtime.Permission;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.runtime.Permission;
+
+
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
 
     private Location location;
     private GoogleApiClient googleApiClient;
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private LocationRequest locationRequest;
-    private static final long UPDATE_INTERVAL = 60000, FASTEST_INTERVAL = 60000; // = 5 seconds
-
+    private static final long UPDATE_INTERVAL = 10000, FASTEST_INTERVAL = 10000; // = 5 seconds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // we build google api client
-        googleApiClient = new GoogleApiClient.Builder(this).
-                addApi(LocationServices.API).
-                addConnectionCallbacks(this).
-                addOnConnectionFailedListener(this).build();
 
         AndPermission.with(MainActivity.this)
                 .runtime()
@@ -61,8 +66,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                 .start();
 
 
-                startLocationUpdates();
+        googleApiClient = new GoogleApiClient.Builder(this).
+                addApi(LocationServices.API).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).build();
+
     }
+
+
+
+
 
 
     @Override
@@ -74,114 +87,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         }
     }
 
-    private void startLocationUpdates() {
-        locationRequest = new LocationRequest();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(UPDATE_INTERVAL);
-        locationRequest.setFastestInterval(FASTEST_INTERVAL);
-
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                &&  ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "You need to enable permissions to display location !", Toast.LENGTH_SHORT).show();
-        }
-
-        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location != null) {
-            Log.e("Ubicación",location.getLatitude() + " | " + location.getLongitude());
-            Toast.makeText(this, location.getLatitude() + " | " + location.getLongitude(), Toast.LENGTH_SHORT).show();
-
-            //String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-            String refreshedToken = "IDENTIFICADOR";         String url = "URL TO SAVE";
-            try {
-                RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
-
-                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, response -> {
-                    Log.e("Resultado", "-> " + response);
-
-                }, error -> Log.e("Resultado", String.valueOf(error))) {
-                    protected Map<String, String> getParams() {
-                        Map<String, String> MyData = new HashMap<String, String>();
-                        MyData.put("lat", String.valueOf(location.getLatitude()));
-                        MyData.put("lng", String.valueOf(location.getLongitude()));
-                        MyData.put("usr", refreshedToken);
-                        return MyData;
-                    }
-                };
-
-                MyStringRequest.setRetryPolicy(new RetryPolicy() {
-                    @Override
-                    public int getCurrentTimeout() {
-                        return 50000;
-                    }
-                    @Override
-                    public int getCurrentRetryCount() {
-                        return 50000;
-                    }
-                    @Override
-                    public void retry(VolleyError error) throws VolleyError {
-                    }
-                });
-
-                MyRequestQueue.add(MyStringRequest);
-            } catch (Exception e) { Log.e("Error Save Location",e.toString()); }
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                &&  ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-
-        if (location != null) {
-
-            String LaUbica = "Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude();
-            Log.e("Ubicación",LaUbica);
-            Toast.makeText(this, LaUbica, Toast.LENGTH_SHORT).show();
-        }
-
-        startLocationUpdates();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-
-
     @Override
     protected void onResume() {
         super.onResume();
@@ -189,6 +94,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         if (!checkPlayServices()) {
             Toast.makeText(this, "You need to install Google Play Services to use the App properly", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     private boolean checkPlayServices() {
@@ -207,4 +117,66 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         return true;
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        // Permissions ok, we get last location
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        if (location != null) {
+
+            String LaUbica = "Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude();
+            Log.e("Ubicación",LaUbica);
+            Toast.makeText(this, LaUbica, Toast.LENGTH_SHORT).show();
+        }
+
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "You need to enable permissions to display location !", Toast.LENGTH_SHORT).show();
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            String LaUbica = location.getLatitude() + " | " + location.getLongitude();
+            Log.e("Ubicación",LaUbica);
+            Toast.makeText(this, LaUbica, Toast.LENGTH_SHORT).show();
+
+            String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+            Log.e("Token",refreshedToken);
+        }
+    }
+
+
+
+
 }
